3b32e0401aada3c1fe18f4c74ef26b31ef97ee45
tomas-martin <tomasmartin4572@gmail.com>
2026-01-06 13:57:54 -0300

    Probando algo

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://sgwchweyuagbmllhzgbo.supabase.co',
   supabaseKey: 'sb_publishable_j_mTda6R4DVLN0wHLjkBBQ_4intfJ3f'
 };
