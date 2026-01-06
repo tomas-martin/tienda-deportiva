1b11edd57a412e3ffa00e15245112e5f6aacc5e2
tomas-martin <tomasmartin4572@gmail.com>
2026-01-06 13:54:52 -0300

    Fin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+  .catch(err => console.error(err));
